576a6d9a9dac0c2cfe51ac5c2af4b4cd577b6dd1
Makhdum Deshmukh <MDeshmukh@vitalhuber.local>
2020-02-06 18:36:58 +0530

    client-app first commit

---
+++ client-app/src/demo.ts
@@ -0,0 +1,30 @@
+import { Interface } from "readline";
+
+let data;
+
+data = "43";
+
+data = 2;
+
+export interface ICar {
+    color: string;
+    model: string;
+    topSpeed?: number;
+}
+const car1: ICar = {
+    color: 'blue',
+    model: 'Bmw'
+}
+
+const car2: ICar = {
+    color: 'red',
+    model: 'Mercedes',
+    topSpeed: 100 
+}
+
+const multiply = (x: number, y: number) => {
+    x*y;
+}
+
+export const cars = [car1, car2];
+//export const ICar= [car1, car2];
